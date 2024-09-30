f6b8990b9aa07677f84486d5310196472048c612
罗嘉淳 <ljcsysu@gmail.com>
2024-09-30 16:02:42 +0800

    Changes

--- app/models/visitor.py
+++ app/models/visitor.py
@@ -6,8 +6,9 @@
 # 版本: 1.0
 # 描述: 访客模型文件。
 """
+from xmlrpc.client import Boolean
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from extensions.db import db
 
 
@@ -15,11 +16,12 @@ from extensions.db import db
 class Visitor(db.Model):
     __tablename__ = 'visitors'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 访客ID
     name = Column(String(50), nullable=False)  # 姓名
     id_type = Column(String(50), nullable=False)  # 证件类型
     id_number = Column(String(100), nullable=False)  # 证件号码
     phone_number = Column(String(20), nullable=False)  # 手机号码
+    is_deleted = Column(Boolean, default=False)  # 是否删除
     user_id = Column(Integer, ForeignKey('users.id'), nullable=True)  # 上一级用户ID
 
     def __repr__(self):
