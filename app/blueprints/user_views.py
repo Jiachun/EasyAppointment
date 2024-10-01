96c91b9ce458f86fc9171114028c14096cbf5428
罗嘉淳 <ljcsysu@gmail.com>
2024-10-01 15:09:25 +0800

    Changes

--- app/blueprints/user_views.py
+++ app/blueprints/user_views.py
@@ -4,7 +4,7 @@
 # 作者: 罗嘉淳
 # 创建日期: 2024-10-01
 # 版本: 1.0
-# 描述: 用户蓝图。
+# 描述: 用户管理蓝图。
 """
 
 
@@ -14,6 +14,9 @@ from app.controllers import UserController
 
 user_blueprint = Blueprint('user_blueprint', __name__)
 
+
+# 模拟用户数据
+users = [{'id': 1, 'name': 'Alice'}, {'id': 2, 'name': 'Bob'}]
 
 @user_blueprint.route('/', methods=['GET'])
 def list_users():
