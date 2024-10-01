c261833aa5cb075dc87b4cd2956539184a0a8c13
罗嘉淳 <ljcsysu@gmail.com>
2024-10-01 18:42:08 +0800

    Changes

--- app/redprints/__init__.py
+++ app/redprints/__init__.py
@@ -1,5 +1,7 @@
 from .user_api import user_api
+from .campus_api import campus_api
 
 
 def register_redprints(app):
     app.register_blueprint(user_api, url_prefix='/api/users')  # 注册用户API
+    app.register_blueprint(campus_api, url_prefix='/api/campuses')  # 注册用户API
