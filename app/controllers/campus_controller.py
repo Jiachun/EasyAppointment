622d68bb5c028a7150a259026c3ff28005502522
罗嘉淳 <ljcsysu@gmail.com>
2024-10-22 09:32:07 +0800

    Changes

--- app/controllers/campus_controller.py
+++ app/controllers/campus_controller.py
@@ -30,6 +30,7 @@ class CampusController:
         # 返回分页后的数据、总页数、当前页和每页记录数
         return format_response(True, {
             "campuses": [campus.to_dict() for campus in paginated_campuses.items],
+            "total": paginated_campuses.count(),
             "total_pages": paginated_campuses.pages,
             "current_page": page,
             "per_page": per_page
@@ -163,6 +164,7 @@ class CampusController:
         # 返回分页后的数据、总页数、当前页和每页记录数
         return format_response(True, {
             "campuses": [campus.to_dict() for campus in paginated_campuses.items],
+            "total": paginated_campuses.count(),
             "total_pages": paginated_campuses.pages,
             "current_page": page,
             "per_page": per_page
